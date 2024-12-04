d06fc90491669d6b57693c00a40b3ee179c8142b
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2024-12-03 19:04:26 -0600

    Routes are working, but we have to hack the URL...
    There's a better way...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { WorldComponent } from './world/world.component';
+import { KittyComponent } from './kitty/kitty.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: ""
+        , component: WorldComponent
+    },
+    {
+        path: "kitty"
+        , component: KittyComponent
+    }
+];
