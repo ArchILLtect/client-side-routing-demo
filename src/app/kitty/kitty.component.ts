f766a095d40a6525af7ddfd4f1ce51e9897cc81e
ArchILLtect <140122527+ArchILLtect@users.noreply.github.com>
2024-12-03 18:56:20 -0600

    Generated two components...
    ng generate component world --skip-tests
    
    ng generate component kitty --skip-tests

---
+++ src/app/kitty/kitty.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kitty',
+  imports: [],
+  templateUrl: './kitty.component.html',
+  styleUrl: './kitty.component.css'
+})
+export class KittyComponent {
+
+}
